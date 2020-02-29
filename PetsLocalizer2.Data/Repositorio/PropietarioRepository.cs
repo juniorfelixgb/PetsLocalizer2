7eef2915024d9a5e6dec2c20a605b344939e9f96
Junior Felix Gervacio Burgos <juniorfelixgb@gmail.com>
2020-02-29 19:02:37 -0400

    Add PropietarioRepository -- 29/2/2020

---
+++ PetsLocalizer2.Data/Repositorio/PropietarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsLocalizer2.Data.Repositorio
+{
+    class PropietarioRepository
+    {
+    }
+}
